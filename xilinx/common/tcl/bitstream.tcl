534d644f28735bda65c690d16ac45f50ad634cd0
zyy <gt7591665@gmail.com>
2026-08-19 10:48:25 +0800

    add generate bin file tcl

--- xilinx/common/tcl/bitstream.tcl
+++ xilinx/common/tcl/bitstream.tcl
@@ -1,7 +1,14 @@
 # See LICENSE for license details.
 
-# Write a bitstream for the current design
-write_bitstream -force [file join $wrkdir "${top}.bit"]
+# Write a bitstream for the current design. Boards which set
+# generate_bin_file also receive a raw configuration image suitable for their
+# SPI configuration flash.
+set bitfile [file join $wrkdir "${top}.bit"]
+if {[info exists generate_bin_file] && $generate_bin_file} {
+	write_bitstream -force $bitfile -bin_file
+} else {
+	write_bitstream -force $bitfile
+}
 
 # Save the timing delays for cells in the design in SDF format
 write_sdf -force [file join $wrkdir "${top}.sdf"]
